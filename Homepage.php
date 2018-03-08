f864ad17901295f4f2be77584c3fffe811bf761d
palak <32808919+palakg11@users.noreply.github.com>
2018-03-08 22:10:49 +0530

    Homepage Added

--- Homepage.php
+++ Homepage.php
@@ -7,40 +7,40 @@
 	<link rel="stylesheet" type="text/css" href="Style/Homepage.css">
 </head>
 <body>
-<?php 
-require_once 'connection.php';
-require_once 'autodelete.php';
-require_once 'login.php';
-require_once 'publicpost.php';
-$user=$pass=$passerr=""; 
-?>
-<h1 class="display-3">PasteBin</h1>
-<div class="login">
-	<form class="loginform" method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
-	<span>Username:</span> <input type="text" name="username" placeholder="Name....">
-	<span>Password:</span> <input type="Password" name="pass1" placeholder="Password...">
-	<input type="submit" name="login" value="login">
-	</form>
-</div>
-<div class="postarea">
-	<div class="signup">
-		<?php include 'signup.php'; ?>
+	<?php 
+	require_once 'connection.php';
+	require_once 'autodelete.php';
+	require_once 'login.php';
+	require_once 'publicpost.php';
+	$user=$pass=$passerr=""; 
+	?>
+	<h1 class="display-3">PasteBin</h1>
+	<div class="login">
+		<form class="loginform" method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
+			<span>Username:</span> <input type="text" name="username" placeholder="Name....">
+			<span>Password:</span> <input type="Password" name="pass1" placeholder="Password...">
+			<input type="submit" name="login" value="login">
+		</form>
 	</div>
-	<div class="publicpastes">
-		<h2>Posts</h2>
-		<table class = "table">
-		<?php
-		while($pastepost=$publicpost->fetch_assoc()) {
-			if($pastepost["Publicposts"]!=NULL){
-				$content = '<tr>
-								<td>'.$pastepost["Username"].'</td>
-								<td>'.$pastepost["Publicposts"].'</td>
-							</tr>
-							';
-				echo $content;
+	<div class="postarea">
+		<div class="signup">
+			<?php include 'signup.php'; ?>
+		</div>
+		<div class="publicpastes">
+			<h2>Posts</h2>
+			<table class = "table">
+				<?php
+				while($pastepost=$publicpost->fetch_assoc()) {
+					if($pastepost["Publicposts"]!=NULL){
+						$content = '<tr>
+						<td>'.$pastepost["Username"].'</td>
+						<td>'.$pastepost["Publicposts"].'</td>
+					</tr>
+					';
+					echo $content;
+				}
 			}
-		}
-		?>
+			?>
 		</table>
 	</div>
 </div>
